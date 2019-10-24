2fd68c567abbbbb1061e3ab3f05b7e4cf17f7fdb
codedubs <35834450+codedubs@users.noreply.github.com>
2019-10-24 01:52:07 -0700

    err

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -1,6 +1,6 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
-const sgMail = require('@sendgrid/mail');
+/* const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 function sendMail(to, subject, text) {
@@ -16,3 +16,4 @@ function sendMail(to, subject, text) {
 }
 
 module.exports = sendMail;
+*/
