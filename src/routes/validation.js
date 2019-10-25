a557953d15c166e118adf7785a107452f0fd673f
codedubs <35834450+codedubs@users.noreply.github.com>
2019-10-25 16:41:09 -0700

    err

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -11,13 +11,13 @@ module.exports = {
     if (req.method === "POST") {
 
       req.checkBody("email", "must be a valid email").isEmail();
-      req.checkBody("password", "must be at least 6 characters in length").isLength({min: 5});
-
+      req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6});
+req.checkBody("passwordConfirmation", "must match password provided").optional().matches(req.body.password);
     }
 
     const errors = req.validationErrors();
 
-    if (errors) { 
+    if (errors) {
       req.flash("error", errors);
       return res.redirect("/users/signup")
     } else {
