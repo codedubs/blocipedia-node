75fa86883ad3c7124515da65ecfa0b0edc5c5e66
codedubs <35834450+codedubs@users.noreply.github.com>
2019-10-24 17:10:51 -0700

    err

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,4 @@
-/* const express = require("express");
+const express = require("express");
 const router = express.Router();
 const validation = require("./validation");
 const userController = require("../controllers/userController");
@@ -12,4 +12,3 @@ router.post("/users/signup", validation.validateUsers, userController.create);
 
 
 module.exports = router;
-*/
