5391bd15cd3434ea4c63a145432f158dcc3c1bdd
codedubs <35834450+codedubs@users.noreply.github.com>
2019-10-25 01:45:03 -0700

    err

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -11,9 +11,9 @@ module.exports = {
     if (req.method === "POST") {
 
       req.checkBody("email", "must be a valid email").isEmail();
-      req.checkBody("password", "must be at least 6 characters in length").isLength({min: 5})
-      req.body('email').custom(value => {
-  return User.findUserByEmail(value).then(user => {
+      req.checkBody("password", "must be at least 6 characters in length").isLength({min: 5});
+      req.body('email').custom(email => {
+  return User.findUserByEmail(email).then(user => {
     if (user) {
       return Promise.reject('E-mail already in use');
     }
