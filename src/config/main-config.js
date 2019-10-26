1caa3d42de3ac90a9ed46ac8598065dfa844b099
codedubs <35834450+codedubs@users.noreply.github.com>
2019-10-25 21:07:11 -0700

    er

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -55,7 +55,7 @@ module.exports = {
 
     app.use(expressValidator());
     app.use(session({
-      secret: process.env.cookieSecret,
+      secret: "process.env.cookieSecret",
       resave: false,
       saveUninitialized: false,
       cookie: {maxAge: 1.21e+9}
