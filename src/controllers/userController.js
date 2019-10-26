89072653256904e02acae02f543e294921a59ba9
codedubs <35834450+codedubs@users.noreply.github.com>
2019-10-25 20:31:30 -0700

    err

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -18,7 +18,7 @@ module.exports = {
 
     userQueries.createUser(newUser, (err, user) => {
       if(err) {
-        req.flash("error", err);
+        req.flash("that email already exists", err);
         res.redirect("/users/signup")
       } else {
         sendMail(user.email, "Welcome to blocipedia", "You've signed up!");
