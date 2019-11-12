665985012def57e7e46d0d423722e6db3c33fa88
codedubs <35834450+codedubs@users.noreply.github.com>
2019-11-12 04:05:34 -0800

    Checkpoint 6 complete

--- src/db/migrations/20191106070739-add-role-to-user.js
+++ src/db/migrations/20191106070739-add-role-to-user.js
@@ -12,6 +12,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('User', 'role');
+    return queryInterface.removeColumn('Users', 'role');
   }
 };
