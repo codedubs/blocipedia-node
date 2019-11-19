3740027d8b5431c5be57a6cf06a5bc311522aea8
codedubs <35834450+codedubs@users.noreply.github.com>
2019-11-19 02:13:21 -0800

    Update to checkpoint 6

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -23,8 +23,7 @@ module.exports = {
     userQueries.createUser(newUser, (err, user) => {
      if(err) {
         req.flash("error", err);
-        res.redirect("/users/signup")
-
+        res.redirect("/users/signup");
       } else {
         passport.authenticate("local")(req, res, () => {
           req.flash("notice", "You've successfully signed up!");
--- src/routes/users.js
+++ src/routes/users.js
@@ -7,7 +7,7 @@ const stripeController = require("../controllers/stripeController");
 
 
 router.get("/users/signup", userController.signUp)
-router.post("/users/signup", /*validation.validateUsers,*/ userController.create);
+router.post("/users/signup", validation.validateUsers, userController.create);
 router.get("/users/sign_in", userController.signInForm);
 router.post("/users/sign_in", validation.validateUsers, userController.signIn);
 router.get("/users/sign_out", userController.signOut);
