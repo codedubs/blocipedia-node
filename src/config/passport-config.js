2256d55eba79bcf982b17094337965514382d6df
codedubs <35834450+codedubs@users.noreply.github.com>
2019-11-18 19:35:29 -0800

    ui

--- src/config/passport-config.js
+++ src/config/passport-config.js
@@ -1,4 +1,4 @@
-const passport = require("passport");
+/*const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const User = require("../db/models").User;
 const authHelper = require("../auth/helpers");
@@ -35,6 +35,48 @@ module.exports = {
         callback(null, user);
       })
       .catch(err => {
+        callback(err, user);
+      })
+    });
+  }
+}*/
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../db/models").User;
+const authHelper = require("../auth/helpers");
+
+
+module.exports = {
+
+  init(app) {
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    passport.use(new LocalStrategy({
+      usernameField: "email"
+    }, (email, password, done) => {
+      User.findOne({
+        where: {email}
+      })
+      .then((user) => {
+        if(!user || !authHelper.comparePass(password, user.password)) {
+          return done(null, false, {message: "Invalid email or password"});
+        }
+        return done(null, user);
+      })
+    }));
+
+    passport.serializeUser((user, callback) => {
+      callback(null, user.id);
+    });
+
+    passport.deserializeUser((id, callback) => {
+      User.findById(id)
+      .then((user) => {
+        callback(null, user);
+      })
+      .catch((err) => {
         callback(err, user);
       })
     });
